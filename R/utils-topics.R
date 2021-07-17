7c0a4d5f971e455ababd780d8c0bb600c89f2671
Stephanie J. Spielman <spielman@rowan.edu>
2021-07-17 17:47:34 -0400

    Comments for the forcats functions to do

--- R/utils-topics.R
+++ R/utils-topics.R
@@ -58,6 +58,18 @@ tidyr_topics <- sort(c(
 ))
 
   
+#' All the forcats topics
+#' @keywords internal
+#forcats_topics <- sort(c(
+  #"fct_relevel",
+  #"fct_rev"
+  #"fct_reorder",
+  #"fct_infreq",
+#### these should be the same page ###
+  #"fct_lump_n", 
+  #"fct_lump_prop",
+  #"fct_lump_min"
+#))
   
 #' All the base R topics
 #' @keywords internal
@@ -99,6 +111,7 @@ topic_list <- list(
   "datasets"  = c("carnivores", "msleep"),
   "dplyr"     = dplyr_topics,
   "ggplot2"   = ggplot2_topics,
+  #"forcats"   = forcats_topics,
   "magrittr"  = c("apipe", "pipe"), # keep assignment first
   "operators" = c("assignment", "logical", "mathematical"), # alphabetical
   "tidyr"     = tidyr_topics
